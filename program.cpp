67b9b8a19807016aba6498aa00f37a24be1ea106
immbianca <bianca.mustea@student.upt.ro>
2024-12-13 11:39:48 +0200

    startimage

--- program.cpp
+++ program.cpp
@@ -1,12 +1,25 @@
 #include<graphics.h>
 #include<windows.h>
 
+
+
 int main()
 {
 	int screenWidth= GetSystemMetrics(SM_CXSCREEN);
 	int screenHeight= GetSystemMetrics(SM_CYSCREEN);
     initwindow(screenWidth, screenHeight, "Turnurie din Hanoi");
-    circle(100, 100, 50);
+	
+	setbkcolor(WHITE);
+	cleardevice();
+	
+    setcolor(BLUE);
+	settextstyle(DEFAULT_FONT,HORIZ_DIR, 3);
+	outtextxy(200,200,"START");
+	outtextxy(200,250,"INSTRUCTIUNI");
+	outtextxy(200,300,"SETARI");
+	outtextxy(200,350,"EXIT");
+	
+	readimagefile("Assets/images/turnuri_v1.jpg",800,200, 900,500);
     getch();
     closegraph();
     return 0;
