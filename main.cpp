a06ecf586507fdd509223bfb70bda8a3da54224a
immbianca <bianca.mustea@student.upt.ro>
2025-01-01 17:13:05 +0200

    instructiuni

--- main.cpp
+++ main.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+void instructiuni(){
+    system("cls");
+    cout<<"\n=== Instructiuni ===\n";
+    cout<<"Scopul jocului este de a muta toate discurile de pe unul dintre turnuri pe celalalt, folosind un al treilea turn ca suport.\n";
+    cout<<"Regulile jocului sunt simple:\n";
+    cout<<"1. Se poate muta cate un disc la un moment dat.\n";
+    cout<<"2. Un disc mai mare nu poate fi plasat peste un disc mai mic.\n";
+    cout<<"3. Se poate muta un disc doar de pe varful unui turn.\n";
+    cout<<"4. Se poate muta un disc doar pe varful unui turn.\n";
+    cout<<"5. Scopul jocului este de a muta toate discurile de pe un turn pe celalalt, folosind un al treilea turn ca suport.\n";
+    cout<<"6. Numarul de mutari este minim daca se respecta regulile de mai sus.\n";
+    cout<<"7. Jocul se termina atunci cand toate discurile sunt pe un alt turn decat cel initial.\n";
+    cout<<"8. Numarul minim de mutari este 2^n - 1, unde n este numarul de discuri.\n";
+    cout<<"9. Jocul se poate juca cu orice numar de discuri.\n";
+    cout<<"10. Succes!\n";
+    cout<<"Apasa orice tasta pentru a reveni la meniu...";
+    cin.get();
+    cin.get();
+}
 void meniu(){
+    system("cls");
     cout<<"====================="<<endl;
     cout<<"Turnutile din Hanoi"<<endl;
     cout<<"====================="<<endl;
@@ -21,6 +41,7 @@ int main(){
             case 1:
                 break;
             case 2:
+                instructiuni();
                 break;
             case 3:
                 cout<<"La revedere!"<<endl;
