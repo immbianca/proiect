5f87dfdd478c7c74703b7c3bf2dcc58ddb4995ba
immbianca <bianca.mustea@student.upt.ro>
2025-01-01 16:55:16 +0200

    meniu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
+
 using namespace std;
-int main()
-{
-    cout << "Hello World!";
+
+void meniu(){
+    cout<<"====================="<<endl;
+    cout<<"Turnutile din Hanoi"<<endl;
+    cout<<"====================="<<endl;
+    cout<<"1. Start joc"<<endl;
+    cout<<"2. Instructiuni"<<endl;
+    cout<<"3. Iesire"<<endl;
+    cout<<"Alegere: ";
+}
+
+int main(){
+    int optiune;
+    do{
+        meniu();
+        cin>>optiune;
+        switch(optiune){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                cout<<"La revedere!"<<endl;
+                break;
+            default:
+                cout<<"Optiune invalida!"<<endl;
+        }
+    }while (optiune!=3);
     return 0;
 }
